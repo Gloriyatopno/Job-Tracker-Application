54a9052c50c3d84ed6955c7b99dad232d18225d0
Gloriya Topno <gloriyatopno8@gmail.com>
2026-07-01 18:44:17 +0530

    Enable CORS for frontend integration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const jobsRoutes = require("./routes/jobsRoutes");
+
 const app = express();
 
-
+app.use(cors());          
 app.use(express.json());
+
 app.use("/jobs", jobsRoutes);
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "Welcome to the Job Tracker API!"
-    });
+  res.status(200).json({
+    success: true,
+    message: "Welcome to the Job Tracker API!",
+  });
 });
 
 module.exports = app;
